88f3abbae7cdfe7bb01d87cec05a373298ce9eaa
jonah odoh <jonahodoh@gmail.com>
2025-01-25 08:34:34 +0100

    my first

---
+++ src/main/java/group3/africa/cropnest/CropNestApplication.java
@@ -0,0 +1,13 @@
+package group3.africa.cropnest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CropNestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CropNestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/group3/africa/cropnest/CropNestApplicationTests.java
@@ -0,0 +1,13 @@
+package group3.africa.cropnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CropNestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
