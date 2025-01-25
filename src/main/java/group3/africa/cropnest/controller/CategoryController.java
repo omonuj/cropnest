3e27d1b45059480d00d2d7f9fcdb78df7faca392
jonah odoh <jonahodoh@gmail.com>
2025-01-25 09:09:37 +0100

    working

---
+++ src/main/java/group3/africa/cropnest/controller/CategoryController.java
@@ -0,0 +1,11 @@
+package group3.africa.cropnest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class CategoryController {
+
+}
