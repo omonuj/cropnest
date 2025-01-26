62a9fb6e6ab1aad731905dc514c5983594cb3177
jonah odoh <jonahodoh@gmail.com>
2025-01-26 17:07:16 +0100

    upate

--- src/main/java/group3/africa/cropnest/Service/ProductService.java
+++
@@ -1,9 +0,0 @@
-package group3.africa.cropnest.Service;
-
-
-import group3.africa.cropnest.Dtos.Request.ProductRequestDTO;
-import group3.africa.cropnest.Dtos.Response.ProductResponseDTO;
-
-public interface ProductService {
-    ProductResponseDTO addProduct(Long categoryId, ProductRequestDTO productRequestDTO);
-}
